709f91998f1726ef432ea838aa5f8a3e7095f120
WLopesF <williamlopesfreitas789@gmail.com>
2024-11-29 15:22:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route('/<nome>')
 
-app.run(host="0.0.0.0")
-
 def saudacao(nome):
     return f"Olá, seu nome é {nome}!"
     
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
